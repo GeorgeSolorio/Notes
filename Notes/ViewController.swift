a6665ea7b7e3eb08a7f544aeeb6e000308b6fee0
GeorgeSolorio <Georgemsolorio@gmail.com>
2020-11-02 13:25:53 -0600

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by George Solorio on 11/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
